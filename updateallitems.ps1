0002501294e3c626e98d4782341877d32ae82d01
Jonatan Kapps <jonatankapps@live.com>
2018-01-09 22:45:20 -0200

    Fix: Changing parameters name

--- updateallitems.ps1
+++ updateallitems.ps1
@@ -7,14 +7,14 @@ Write-Host "#                                                      #"
 Write-Host "########################################################"
 
 
-function UpdateAllListItems([string]$url, [string]$list){
+function UpdateAllListItems([string]$url, [string]$listTitle){
     Write-Host "Loading $url site"
     $site = Get-SPSite $url
     $rootWeb = $site.rootWeb
     
     Write-Host "Loading $list list"
     $lists = $rootWeb.Lists
-    $list = $lists | Where-Object {$_.Title -eq $list}
+    $list = $lists | Where-Object {$_.Title -eq $listTitle}
     
     if ($list -ne $null) {
         Write-Host "List found, updating items..."
